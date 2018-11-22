529ccdee1668eb6259c6de53c64a7d939c59d911
lbf <648397687@qq.com>
2018-11-22 10:03:31 +0800

    first commit

---
+++ src/utils/constant.js
@@ -0,0 +1,52 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+//是否商户管理员
+export const BE_SHOPADMIN = "shopAdmin";
+
+// 用户token
+export const TOKEN = "token";
+
+// custId
+export const CUSTID = "custId";
+
+//openId
+export const OPENID = "openId";
+
+//SHOPID
+export const SHOPID = "shopId";
+
+//SHOPPHONE
+export const SHOPPHONE = "shopPhone";
+
+//分类选择itemcode
+export const ACTIVE_CODE = "activecode";
+
+//属性选择itemcode
+export const ATTR_ACTIVE_CODE = "attractivecode";
+
+//购物车z
+export const SHOP_CART = 'shopcart';
+
+//订单详情
+export const ORDER_DETAIL = "orderDetail";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+export const ADDRESS_ID = "addressId";
+
+export const SEL_CLASS_CODE = "selClassCode";
+
